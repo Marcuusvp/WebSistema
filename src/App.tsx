76870730d222a7c3cacdae051245096c3e42b1e4
Marcuusvp <marcusviniciuspereira1@gmail.com>
2023-02-02 22:46:50 -0300

    feat:configurado tema no material

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
