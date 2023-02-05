14761c82745b84b2a4fb970012b5d8aec20b3c30
Marcuusvp <marcusviniciuspereira1@gmail.com>
2023-02-05 14:48:52 -0300

    instalado o unform para trabalhar com formulários e inicio da construcao do custom text field

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,22 +1,57 @@
+import { LinearProgress } from '@mui/material';
+import { Form } from '@unform/web';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FerramentasDeDetalhe } from '../../shared/components';
+import { VTextField } from '../../shared/forms';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/pessoas/PessoaServices';
 
 export const DetalheDePessoas: React.FC = () => {
-  const {id = 'nova'} = useParams<'id'>();
+  const {id = 'cadastro'} = useParams<'id'>();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+  
+  useEffect(() =>{
+    if(id !== 'cadastro'){
+      setIsLoading(true);
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false);
+          if(result instanceof Error){
+            alert(result.message);
+            navigate('/pessoas');
+          } else{
+            setNome(result.nomeCompleto);
+            console.log(result);
+          }
+        });
+    }
+    return;
+  },[id]);
 
   const handleSave = () => {
     console.log('opa');
   };
 
-  const handleDelete = () => {
-    console.log('epa');
+  const handleDelete = (id: number) => {
+    if(confirm('Deseja deletar esse cliente?')){
+      PessoasService.deleteById(id)
+        .then(result => {
+          if(result instanceof Error){
+            alert(result.message);
+          }else{            
+            alert('Registro apagado com sucesso');
+            navigate('/pessoas');
+          }
+        });
+    }
   };
 
   return(
     <LayoutBaseDePagina
-      titulo='Cadastrar'
+      titulo={id ==='cadastro'? 'Cadastrar Cliente': nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo='Cadastrar Cliente'
@@ -26,13 +61,19 @@ export const DetalheDePessoas: React.FC = () => {
           
           aoClicarEmSalvar={() => handleSave}
           aoClicarEmSalvarEFechar={() => handleSave}
-          aoClicarEmApagar={() => handleDelete}
+          aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmNovo={() => navigate('/pessoas/detalhe/cadastro')}
           aoClicarEmVoltar={() => navigate('/pessoas')}
         />
       }
     >
-
+      <Form onSubmit={console.log}>
+        <VTextField
+          name='nomeCompleto'/>
+      </Form>
+      {isLoading &&(
+        <LinearProgress variant='indeterminate'/>
+      )}
     </LayoutBaseDePagina>
   );
 };
---
+++ src/shared/forms/VTextField.tsx
@@ -0,0 +1,38 @@
+import { TextField, TextFieldProps } from '@mui/material';
+import { useField } from '@unform/core';
+import { useEffect, useState } from 'react';
+
+//type para habilitar a possibilidade de receber as props do TextField do MUI mais as nossas novas props customizadas.
+type TVTextFieldProps = TextFieldProps &{
+  name: string;
+};
+/**
+ * Componente customizado, baseado no Material-UI
+ */
+export const VTextField: React.FC<TVTextFieldProps> = ({ name, ...rest }) => {
+  const { fieldName, registerField, defaultValue, error, clearError} = useField(name);
+  const [value, setValue] = useState(defaultValue || '');
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => value,
+      setValue: (e, newValue) => setValue(newValue),
+    });
+  },[registerField, fieldName, value]);
+
+  return(
+    <TextField
+      {...rest}
+
+      error={!!error}
+      helperText={error}
+      defaultValue={defaultValue}
+
+      value={value}
+      onChange={e => setValue(e.target.value)}
+
+      onKeyDown={() => error? clearError() : undefined}
+    />
+  );
+};
